62c818290c88c402425c66058e5b58cfbd5ea6d9
Felipesv1 <fepereirasilva4@gmail.com>
2024-07-26 15:02:26 -0300

    add diretorios cpp

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string.h>
+
+int main(int ac, char **av) {
+
+    if (ac == 1)
+    {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+    }
+    else
+    {
+        for (int i = 1; i < ac; i++)
+        {
+            for (int j = 0; j < strlen(av[i]); j++)
+            {
+                std::cout << (char)toupper(argv[i][j]);
+            }
+        }
+        std::cout << std::endl;
+    }
+    return 0;
+}
---
+++ ex01/PhoneBook.cpp
@@ -0,0 +1,15 @@
+#include <string>
+class PhoneBook {
+public:
+    void add(const std::string& name, const std::string& number) {
+        // Add the name and number to the phone book
+    }
+
+    std::string get(const std::string& name) {
+        // Return the number associated with the name
+    }
+
+    void remove(const std::string& name) {
+        // Remove the name and number from the phone book
+    }
+};
